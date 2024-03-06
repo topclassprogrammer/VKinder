2344d3a0ee8ca8a9437344af0dc02f4fc1e3b031
topclassprogrammer <147296984+topclassprogrammer@users.noreply.github.com>
2024-03-06 23:48:54 +0300

    Update const.py

--- const.py
+++ const.py
@@ -9,5 +9,7 @@ PASSWORD = env.int('PASSWORD')
 HOST = env('HOST')
 PORT = env.int('PORT')
 DB_NAME = env('DB_NAME')
+GROUP_TOKEN = env('GROUP_TOKEN')
+USER_TOKEN = env('USER_TOKEN')
 
 Base = declarative_base()
