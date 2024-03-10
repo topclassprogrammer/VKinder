7e5ba5f2e3d8bc473120b549ff2829e0bd942fc8
topclassprogrammer <147296984+topclassprogrammer@users.noreply.github.com>
2024-03-10 18:11:10 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,71 +4,42 @@ from sqlalchemy.orm import relationship
 from const import Base
 
 
-class VkUsers(Base):
-    __tablename__ = 'vk_users'
-    vk_user_id = sq.Column(sq.Integer, primary_key=True)
-    first_name = sq.Column(sq.String(60), nullable=False)
-    last_name = sq.Column(sq.String(60), nullable=False)
-    vk_id = sq.Column(sq.Integer, nullable=False)
-    link_to_photo_1 = sq.Column(sq.String(255))
-    link_to_photo_2 = sq.Column(sq.String(255))
-    link_to_photo_3 = sq.Column(sq.String(255))
-    table_update_time = sq.Column(sq.DateTime, nullable=False)
-    bot_user = relationship('VkBotUsers', back_populates='vk_user')
-
-
-class VkBotUsers(Base):
-    __tablename__ = 'vk_bot_users'
-    vk_bot_users_id = sq.Column(sq.Integer, primary_key=True)
-    bot_user_id = sq.Column(sq.Integer, sq.ForeignKey('bot_users.bot_user_id', ondelete="CASCADE"))
-    vk_user_id = sq.Column(sq.Integer, sq.ForeignKey('vk_users.vk_user_id', ondelete="CASCADE"))
-    vk_user = relationship('VkUsers', back_populates='bot_user')
-    bot_user = relationship('BotUsers', back_populates='vk_user')
-
-
-class BotUsers(Base):
-    __tablename__ = 'bot_users'
-    bot_user_id = sq.Column(sq.Integer, primary_key=True)
+class Bot(Base):
+    """Таблица пользователей бота"""
+    __tablename__ = 'bot'
+    bot_id = sq.Column(sq.Integer, primary_key=True)
     sex = sq.Column(sq.String(15), nullable=False)
     age = sq.Column(sq.Integer, nullable=False)
     city_id = sq.Column(sq.Integer, nullable=False)
-    vk_id = sq.Column(sq.Integer, unique=True)
-    table_update_time = sq.Column(sq.DateTime, nullable=False)
-    vk_user = relationship('VkBotUsers', back_populates='bot_user')
-    favourite = relationship('BotUsersFavourites', back_populates='bot_user')
-    black_list = relationship('BotUsersBlackLists', back_populates='bot_user')
+    profile = sq.Column(sq.Integer, unique=True)
+    update_time = sq.Column(sq.DateTime, nullable=False)
+    search = relationship('BotSearch', back_populates='bot')
 
 
-class BotUsersFavourites(Base):
-    __tablename__ = 'bot_users_favourites'
-    bot_users_favourites_id = sq.Column(sq.Integer, primary_key=True)
-    bot_user_id = sq.Column(sq.Integer, sq.ForeignKey('bot_users.bot_user_id', ondelete="CASCADE"))
-    favourite_id = sq.Column(sq.Integer, sq.ForeignKey('favourites.favourite_id', ondelete="CASCADE"))
-    bot_user = relationship('BotUsers', back_populates='favourite')
-    favourite = relationship('Favourites', back_populates='bot_user')
+class BotSearch(Base):
+    """Промежуточная таблица при отношении многие-ко-многим для связи пользователей бота
+     и найденных результатов ботом"""
+    __tablename__ = 'bot_search'
+    bot_search_id = sq.Column(sq.Integer, primary_key=True)
+    bot_id = sq.Column(sq.Integer, sq.ForeignKey('bot.bot_id', ondelete="CASCADE"))
+    search_id = sq.Column(sq.Integer, sq.ForeignKey('search.search_id', ondelete="CASCADE"))
+    search = relationship('Search', back_populates='bot')
+    bot = relationship('Bot', back_populates='search')
 
 
-class Favourites(Base):
-    __tablename__ = 'favourites'
-    favourite_id = sq.Column(sq.Integer, primary_key=True)
-    vk_id = sq.Column(sq.Integer, unique=True)
-    bot_user = relationship('BotUsersFavourites', back_populates='favourite')
-
-
-class BotUsersBlackLists(Base):
-    __tablename__ = 'bot_users_black_lists'
-    bot_users_black_lists_id = sq.Column(sq.Integer, primary_key=True)
-    bot_user_id = sq.Column(sq.Integer, sq.ForeignKey('bot_users.bot_user_id', ondelete="CASCADE"))
-    black_list_id = sq.Column(sq.Integer, sq.ForeignKey('black_lists.black_list_id', ondelete="CASCADE"))
-    bot_user = relationship('BotUsers', back_populates='black_list')
-    black_list = relationship('BlackLists', back_populates='bot_user')
-
-
-class BlackLists(Base):
-    __tablename__ = 'black_lists'
-    black_list_id = sq.Column(sq.Integer, primary_key=True)
-    vk_id = sq.Column(sq.Integer, unique=True)
-    bot_user = relationship('BotUsersBlackLists', back_populates='black_list')
+class Search(Base):
+    """Таблица найденных результатов ботом"""
+    __tablename__ = 'search'
+    search_id = sq.Column(sq.Integer, primary_key=True)
+    first_name = sq.Column(sq.String(60), nullable=False)
+    last_name = sq.Column(sq.String(60), nullable=False)
+    profile = sq.Column(sq.Integer, nullable=False)
+    photo_1 = sq.Column(sq.String(255))
+    photo_2 = sq.Column(sq.String(255))
+    photo_3 = sq.Column(sq.String(255))
+    is_in_favourite_list = sq.Column(sq.Boolean)
+    is_in_black_list = sq.Column(sq.Boolean)
+    bot = relationship('BotSearch', back_populates='search')
 
 
 def create_tables(engine):
